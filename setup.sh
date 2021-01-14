fb25a3e0d2de318da1aeaeba99f85f5bef5a4ab4
davisobatista <38305853+davisobatista@users.noreply.github.com>
2021-01-13 23:47:06 -0300

    aaaaa

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
-mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"davi.batista@delga.com.br\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+mkdir -p ~/.streamlit
+
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
